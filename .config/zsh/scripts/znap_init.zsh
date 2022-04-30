8886ede049b9038121de4b3a06e0353ff4eadf9f
Jacob Birkett <jacob@birkett.dev>
2022-04-30 01:45:30 -0700

    Format znap_init.zsh

--- .config/zsh/scripts/znap_init.zsh
+++ .config/zsh/scripts/znap_init.zsh
@@ -1,5 +1,5 @@
 [[ -z "$ZNAP_PLUGINS_DIRECTORY" ]] &&
-  ZNAP_PLUGINS_DIRECTORY="${XDG_CONFIG_HOME:-"$HOME/.config"}/zsh/plugins"
+  ZNAP_PLUGINS_DIRECTORY="${XDG_CONFIG_HOME:-$HOME/.config}/zsh/plugins"
 
 [[ ! -d "$ZNAP_PLUGINS_DIRECTORY" ]] &&
   mkdir -p "$ZNAP_PLUGINS_DIRECTORY"
@@ -8,15 +8,15 @@
   ZNAP_PLUGINS_LASTUPDATE_FILE="$ZNAP_PLUGINS_DIRECTORY/.lastupdate"
 
 [[ ! -f "$ZNAP_PLUGINS_LASTUPDATE_FILE" ]] &&
-  echo 0 > "$ZNAP_PLUGINS_LASTUPDATE_FILE"
+  echo 0 >"$ZNAP_PLUGINS_LASTUPDATE_FILE"
 
 [[ -z "$ZNAP_PLUGINS_AUTOUPDATE_INTERVAL" ]] &&
-  ZNAP_PLUGINS_AUTOUPDATE_INTERVAL=$((7*24*60*60)) # every seven days
+  ZNAP_PLUGINS_AUTOUPDATE_INTERVAL=$((7 * 24 * 60 * 60)) # every seven days
 
 local znap="$ZNAP_PLUGINS_DIRECTORY/zsh-snap/znap.zsh"
 
 [[ ! -f "$znap" ]] &&
-  git clone 'https://github.com/marlonrichert/zsh-snap' "$(dirname "$znap")" 
+  git clone 'https://github.com/marlonrichert/zsh-snap' "$(dirname "$znap")"
 
 source "$znap"
 
@@ -26,6 +26,5 @@ local timetarget=$((lastupdate + ZNAP_PLUGINS_AUTOUPDATE_INTERVAL))
 
 if [[ "$timenow" -ge "$timetarget" ]]; then
   znap pull
-  echo "$timenow" > "$ZNAP_PLUGINS_LASTUPDATE_FILE"
+  echo "$timenow" >"$ZNAP_PLUGINS_LASTUPDATE_FILE"
 fi
-
